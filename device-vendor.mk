5d1eeef76aba13a8e8d113ba73cb9f1044ce93aa
haibiao.lu <haibiao.lu@t2mobile.com>
2022-05-14 15:50:30 +0800

    Add system properties related to device information display
    Issue: FP4S-13
    Issue: FP4-A14#16
    Change-Id: I67552c7bb830c9357add7aec1749db03283ea850

--- device-vendor.mk
+++ device-vendor.mk
@@ -12,3 +12,12 @@ PRODUCT_PACKAGES += \
     Launcher3ResCommon \
     VendorFrameworksResCommon \
     VendorSettingsResCommon
+
+
+# T2M custom Device specific properties
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.tct.boot.ver="BTW1Z0Z8TA00" \
+    ro.tct.modem.ver="NTW1Z0Z8TA00" \
+    ro.tct.rec.ver="RTW1Z0Z8TA00" \
+    ro.tct.study.ver="STW1Z0Z8TA00" \
+    ro.tct.sys.ver="YTW1Z0Z8TA00"
