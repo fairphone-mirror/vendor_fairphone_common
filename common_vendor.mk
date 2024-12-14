6cdc439f38d5dd2a348b84440c9f8ade727bcb6e
liquan.zhou <liquan.zhou@t2mobile.com>
2024-12-14 16:53:39 +0800

    FPS-54 [2] tctd diag related feature
    * Comment: tctd porting
    * Root Cause: Requirement
    * Solution: Porting tctd code
    * Test Steps: Build pass
    * Test Result: Build pass
    
    Change-Id: Ia4d1c9fb44a5862bf12cd2536de580e26f32f7e3

--- common_vendor.mk
+++ common_vendor.mk
@@ -4,4 +4,6 @@ ifeq ($(TARGET_BUILD_MMITEST),true)
 PRODUCT_PROPERTY_OVERRIDES += dev.fp.MMITest=true
 endif
 
+PRODUCT_PACKAGES += tctd mm-audio-ftm tdc trace_util
+
 include device/fairphone/common/sepolicy/SEPolicy_vendor.mk
