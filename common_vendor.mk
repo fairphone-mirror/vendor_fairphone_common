b07cdfd9f1446e810ecf3f46a5b2dc5f40218489
liquan.zhou <liquan.zhou@t2mobile.com>
2025-01-02 18:04:37 +0800

    FPS-55 [2]Porting diag_service and diag_client
    * Comment: Porting diag_service and diag_client
    * Root Cause: Requirement
    * Solution: Porting code
    * Test Steps: Check diag commands
    * Test Result: Build pass
    
    Change-Id: I7b9decc3dfefe56cef28a8a738e5a49f2561c38e

--- common_vendor.mk
+++ common_vendor.mk
@@ -5,5 +5,7 @@ PRODUCT_PROPERTY_OVERRIDES += dev.fp.MMITest=true
 endif
 
 PRODUCT_PACKAGES += tctd mm-audio-ftm tdc trace_util
+PRODUCT_PACKAGES += diag_service diag_client
+
 
 include device/fairphone/common/sepolicy/SEPolicy_vendor.mk
