d2da1369f09b021bae99cbe300e1c0825d4f25d0
liquan.zhou <liquan.zhou@t2mobile.com>
2025-02-27 18:09:32 +0800

    FPS-890 Device cannot boot up after flash with the debug ramdisk
    * Comment: The debug ramdisk (vendor_boot-debug.img) is used to enable
      "adb root" on user build. After it is flashed, the "init" will load
      userdbeug system sepolicy form /debug_ramdisk/userdebug_plat_sepolicy.cil.
      But the userdebug_plat_sepolicy.cil is from vendor(A11), this file is
      not new enough. So the device can't boot due to sepolicy issue.
    * Root Cause: Google bug
    * Solution: Incorporates the up-to-date userdebug_plat_sepolicy.cil file
      in its system_ext.img
    * Test Steps: fastboot flash vendor_boot vendor_boot-debug.img and
      reboot
    * Test Result: Pass | Build pass
    
    Change-Id: I4405c133606918524ac847d2d838274d495e36a4

--- common_qssi.mk
+++ common_qssi.mk
@@ -1,3 +1,9 @@
 include device/fairphone/common/sepolicy/SEPolicy_qssi.mk
 
 PRODUCT_PACKAGES += fpconfig_util
+
+# Install a copy of the debug policy to the system_ext partition, and allow
+# init-second-stage to load debug policy from system_ext.
+# This option is only meant to be set by compliance GSI targets.
+PRODUCT_INSTALL_DEBUG_POLICY_TO_SYSTEM_EXT := true
+PRODUCT_PACKAGES += system_ext_userdebug_plat_sepolicy.cil
