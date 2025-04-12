86ee7a5281c25c26aa38b4f4ae211e6fbad991fd
Bharath <bharath@teamb58.org>
2025-04-12 14:56:27 +0530

    overlay: Introduce empty overlays for Frameworks and Settings
    This acts as a base for further customizations.
    
    Issue: FP4-A15#83
    Change-Id: I293965cd50738ef143c238d8aa18440525555f55

--- device-vendor.mk
+++ device-vendor.mk
@@ -1,2 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (C) 2024 FairPhone B.V.
+
+
+# Vendor overlays
+PRODUCT_PACKAGES += \
+    VendorFrameworksResCommon \
+    VendorSettingsResCommon
