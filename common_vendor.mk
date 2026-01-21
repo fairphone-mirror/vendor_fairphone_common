daff23672648ef26966cbffde503cbb36d01525c
liquan.zhou <liquan.zhou@t2mobile.com>
2026-01-21 09:58:00 +0800

    FPSW-729 [FPSW][1]Disable Dump and unnecessary log in RC branch
    * Comment: Disable Dump and unnecessary log in RC branch
    * Root Cause: Requirement
    * Solution: Disable ramdump feature
    * Commit Reason: TASK
    * Test Steps:
        1. Flash user build
        2. Unlock device and flash vendor_boot-debug.img
        3. Use adb root ; adb shell "echo c > proc/sysrq-trigger" to trigger a crash
        4. The DUT should reboot
    * Test Result: Pass
    
    Change-Id: I88921f171ea579e4da49f88e7c86e8525e24962d

--- common_vendor.mk
+++ common_vendor.mk
@@ -19,3 +19,7 @@ PRODUCT_COPY_FILES += \
 
 include device/fairphone/common/sepolicy/SEPolicy_vendor.mk
 include vendor/focaltech/fingerprint/sepolicy.mk
+
+ifeq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.ssr.restart_level=ALL_ENABLE
+endif
